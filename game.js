bb6f2daffde53448d39045d1f558225e6698b9c1
Allen Nguyen <anhn321@gmail.com>
2022-11-17 20:01:49 -0800

    Complete playRound function

--- game.js
+++ game.js
@@ -6,6 +6,40 @@ function getComputerChoice() {
 	return computerChoice;
 }
 
-function playRound(playerSelection, computerChoice) {}
+function playRound(playerSelection, computerSelection) {
+	playerSelection = playerSelection.toLowerCase();
+	playerSelection =
+		playerSelection[0].toUpperCase() + playerSelection.slice(1);
+
+	let returnString = "";
+
+	if (playerSelection === computerSelection) {
+		returnString = `Tie! Both chose ${playerSelection}.`;
+	} else if (playerSelection === "Rock") {
+		if (computerSelection === "Paper") {
+			returnString = `You lose! ${computerSelection} beats ${playerSelection}.`;
+		} else if (computerSelection === "Scissors") {
+			returnString = `You win! ${playerSelection} beats ${computerSelection}.`;
+		}
+	} else if (playerSelection === "Paper") {
+		if (computerSelection === "Rock") {
+			returnString = `You win! ${playerSelection} beats ${computerSelection}.`;
+		} else if (computerSelection === "Scissors") {
+			returnString = `You lose! ${computerSelection} beats ${playerSelection}.`;
+		}
+	} else if (playerSelection === "Scissors") {
+		if (computerSelection === "Rock") {
+			returnString = `You lose! ${computerSelection} beats ${playerSelection}.`;
+		} else if (computerSelection === "Paper") {
+			returnString = `You win! ${playerSelection} beats ${computerSelection}.`;
+		}
+	}
+
+	return returnString;
+}
 
 function game() {}
+
+const playerSelection = "papEr ";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
