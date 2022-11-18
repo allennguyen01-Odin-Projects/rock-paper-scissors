7e9f801ac0b7c7d0b25cae4968d1b82b78789a42
Allen Nguyen <anhn321@gmail.com>
2022-11-17 20:45:05 -0800

    Player can play game in console.

--- game.js
+++ game.js
@@ -12,34 +12,78 @@ function playRound(playerSelection, computerSelection) {
 		playerSelection[0].toUpperCase() + playerSelection.slice(1);
 
 	let returnString = "";
+	let outcome = "";
 
 	if (playerSelection === computerSelection) {
-		returnString = `Tie! Both chose ${playerSelection}.`;
+		returnString = `You tie! Both chose ${playerSelection}.`;
+		outcome = "T";
 	} else if (playerSelection === "Rock") {
 		if (computerSelection === "Paper") {
 			returnString = `You lose! ${computerSelection} beats ${playerSelection}.`;
+			outcome = "L";
 		} else if (computerSelection === "Scissors") {
 			returnString = `You win! ${playerSelection} beats ${computerSelection}.`;
+			outcome = "W";
 		}
 	} else if (playerSelection === "Paper") {
 		if (computerSelection === "Rock") {
 			returnString = `You win! ${playerSelection} beats ${computerSelection}.`;
+			outcome = "W";
 		} else if (computerSelection === "Scissors") {
 			returnString = `You lose! ${computerSelection} beats ${playerSelection}.`;
+			outcome = "L";
 		}
 	} else if (playerSelection === "Scissors") {
 		if (computerSelection === "Rock") {
 			returnString = `You lose! ${computerSelection} beats ${playerSelection}.`;
+			outcome = "L";
 		} else if (computerSelection === "Paper") {
 			returnString = `You win! ${playerSelection} beats ${computerSelection}.`;
+			outcome = "W";
 		}
 	}
 
-	return returnString;
+	return [returnString, outcome];
 }
 
-function game() {}
+function game() {
+	let playerScore = 0;
+	let computerScore = 0;
 
-const playerSelection = "papEr ";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+	for (let i = 1; i <= 5; i++) {
+		console.log(`Round ${i}`);
+		const playerSelection = prompt("What's your move?");
+		console.log(`Player's move: ${playerSelection}.`);
+		const computerSelection = getComputerChoice();
+		console.log(`Computer's move: ${computerSelection}.`);
+
+		const [returnString, outcome] = playRound(
+			playerSelection,
+			computerSelection
+		);
+		console.log(returnString);
+
+		switch (outcome) {
+			case "W":
+				playerScore++;
+				break;
+			case "L":
+				computerScore++;
+			default:
+				break;
+		}
+	}
+
+	let winnerString = "";
+
+	if (playerScore > computerScore) {
+		winnerString = `You win! Score: ${playerScore}-${computerScore}`;
+	} else if (computerScore > playerScore) {
+		winnerString = `You lose! Score: ${computerScore}-${playerScore}`;
+	} else {
+		winnerString = `You tie! Score: ${playerScore}-${computerScore}`;
+	}
+	console.log(winnerString);
+}
+
+game();
