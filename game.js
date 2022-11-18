73d83e3cff258c478986b109257ab962e59682ca
Allen Nguyen <anhn321@gmail.com>
2022-11-17 19:13:47 -0800

    Complete the getComputerChoice function

--- game.js
+++ game.js
@@ -1,4 +1,10 @@
-function getComputerChoice() {}
+function getComputerChoice() {
+	const CHOICES = ["Rock", "Paper", "Scissors"];
+	let randomChoice = Math.floor(Math.random() * CHOICES.length);
+	let computerChoice = CHOICES[randomChoice];
+
+	return computerChoice;
+}
 
 function playRound(playerSelection, computerChoice) {}
 
